f74a21e88b2fe22cf5d49bf5954cf159b362ab60
vaishnavi dade <78514619+vaishnavi8977@users.noreply.github.com>
2025-07-04 13:37:56 -0500

    tested langchain, LLM & fastapi set up

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app = FastAPI(title="AI Text Summarizer")
+
+class SummarizerRequest(BaseModel):
+    text: str
+    model: str
+
+@app.post("/summarize")
+async def summarizer(request: SummarizerRequest):
+    return {"summary": f"Placeholder summary for text: {request.txt} using model: {request.model}"}
