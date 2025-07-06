f577f2d84db569923e512e285d6359d031bad05e
vaishnavi dade <78514619+vaishnavi8977@users.noreply.github.com>
2025-07-06 11:39:28 -0500

    added health check api

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ class SummarizerRequest(BaseModel):
     text: str
     model: str
 
+# health check get API if app is running or not
+@app.get("/health")
+async def healthcheck():
+    return ("App is running")
+
+# post api to summarixe the text
 @app.post("/summarize")
 async def summarizer(request: SummarizerRequest):
     return {"summary": f"Placeholder summary for text: {request.text} using model: {request.model}"}
