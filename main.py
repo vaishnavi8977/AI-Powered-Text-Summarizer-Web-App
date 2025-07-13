ff13352de5516256ab320d9decb6c4305b1d3f89
vaishnavi dade <78514619+vaishnavi8977@users.noreply.github.com>
2025-07-13 01:20:16 -0500

    dynamic web page to show all blog posts

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel, ValidationError
 from dotenv import load_dotenv
 import os
+import jinja2
 from models import SummarizerRequest, BlogPost
 from db import db
 from starlette.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
 
 import creator
 from creator import *
 
 load_dotenv()
+templates = Jinja2Templates(directory="templates")
+
 app = FastAPI(title="AI Text Summarizer")
 
 # health check get API if app is running or not
@@ -52,6 +56,6 @@ async def summarizer(req:SummarizerRequest):
     return JSONResponse(status_code=200, content=dbresult)
 
 @app.get("/blog_list")
-async def get_blog_list():
+async def get_blog_list(request: Request):
     list_of_blogs = await db.get_blog_posts()
-    return JSONResponse(status_code=200, content={"blogs": list_of_blogs})
+    return templates.TemplateResponse("posts.html", {"request":request, "posts": list_of_blogs})
